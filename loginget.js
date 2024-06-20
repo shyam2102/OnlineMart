672b4b52044c7b2f8c339151ff37c4fe0aa59b7c
Shyam Sundar R <112073912+shyam2102@users.noreply.github.com>
2024-06-21 02:30:05 +0530

    Add files via upload

---
+++ loginget.js
@@ -0,0 +1,18 @@
+﻿﻿http=require('http');
+url=require('url');
+querystring = require('querystring');
+
+function onRequest(request,response)
+{
+	var path = url.parse(request.url).pathname;
+	console.log('Request for '+path+' received.');
+	var query =url.parse(request.url).query;
+	console.log(query);
+    var fname = querystring.parse(query)["FName"];
+    var lname = querystring.parse(query)["LName"];
+	var email=querystring.parse(query)["email"];
+	response.write(`Hello ${fname}${lname}, your email id ${email} has been registered successfully`);
+	response.end();
+}
+http.createServer(onRequest).listen(8000);
+console.log('Server has Started…….');
